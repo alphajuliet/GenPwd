f2b3c3b47bb872f4dfdfe74d36eab2c62d98110d
AndrewJ <web@alphajuliet.com>
2016-06-25 11:48:30 +1000

    Add new GenPwd namespace. Encapsulate domain-specific functions into the GenPwd namespace.

--- build/GenPwd.js
+++ build/GenPwd.js
@@ -23,8 +23,6 @@ var Info = {
   }
 }
 
-var nwords = 10;
-
 // Return a random element from an array
 Array.prototype.randomElement = function () {
   return _.nth(this, _.random(0, this.length-1));
@@ -41,38 +39,50 @@ var randomNumericString = function () {
   return _.padStart(_.random(0, 99), 2, "0")
 }
 
-// List generators returning functions to call
-var RandomList = function (t) {
-  return function () {
-    return t.randomElement();
-  }
-}
-
-var WeightedList = function (t) {
-  var expandedList = [];
-  _.forEach(t, function (value, key) {
-    doTimes(value, function () {
-      expandedList.push(key);
-    });
-  });
-  return function () {
-    return expandedList.randomElement();
-  }
-}
-
 //---------------------------------
 // Namespace: GenPwd
+var GenPwd = {
+
+  nwords: 10,
+
+  // List generators returning functions to call
+  RandomList: function (t) {
+    return function () {
+      return t.randomElement();
+    }
+  },
+
+  WeightedList: function (t) {
+    var expandedList = [];
+    _.forEach(t, function (value, key) {
+      doTimes(value, function () {
+        expandedList.push(key);
+      });
+    });
+    return function () {
+      return expandedList.randomElement();
+    }
+  },
+
+  initialise: function () {
+    Info.appendTo("header");
+  }		
+}
+
+
+//---------------------------------
+// Namespace: Generator
 var Generator = {
 
   // Generators
   generator1: {	
-    c1: WeightedList({"b":2,"bl":1,"br":1,"c":2,"cr":1,"ch":2,"cl":1,"d":2,"f":2,"fl":1,"fr":1,"g":2,"gl":1,"gr":1,"h":1,"j":2,"k":2,"l":2,"m":3,"n":2,"p":2,"pl":1,"pr":1,"qu":1,"r":2,"s":3,"sh":2,"sk":1,"sl":1,"sm":1,"sn":1,"st":2,"str":1,"t":3,"th":2,"thr":1,"tr":2,"tw":1,"v":2,"w":1,"z":2}),
-    c2: RandomList(["b","bl","br","cr","ch","cl","d","f","fl","fr","g","gg", "gl","gr","h","j","k","l","m","n","p","pl","pp","pr","pt","qu","r","s","sh","sk","sl","sm","sn","st","str","t","th","thr","tr","tw","v","w","z"]),
-    c3: WeightedList({"b":1,"ch":1,"ck":1,"ct":1,"d":2,"dd":1,"f":1,"ff":1,"ft":1,"g":1,"k":1,"l":2,"ll":1,"lb":1,"ld":1,"lm":1,"ln":1,"lp":1,"lt":1,"m":3,"mp":1,"mt":1,"n":3,"nd":1,"ng":1,"nn":1,"nt":1,"p":2,"pp":1,"pt":1,"rd":1,"rg":1,"rk":1,"rn":1,"rr":1,"rs":1,"rt":1,"s":3,"sh":1,"ss":2,"st":2,"t":3,"tt":2,"th":2,"v":2,"wn":1}),
-    v1: WeightedList({"a":5,"aa":1,"ai":1,"e":5,"ea":1,"ee":1,"i":5,"o":5,"oo":2,"u":2}),
-    v2: WeightedList({"a":5, "e":5,"i":5, "ia":1, "o":5, "oa":1,"oo":2, "u":2, "ua":1}),
-    v3: WeightedList({"a":5,"ao":1,"e":5,"ea":1,"ee":2,"eo":1,"i":2,"ia":2,"io":2,"o":5,"oa":2,"oo":2,"ow":2,"ua":1,"uo":1,"y":5}),
-    p:  RandomList(["!","#","$","^","*","&", "+","@","-","=","/","~","?","\\","%","[","]","{","}","(",")"]),
+    c1: GenPwd.WeightedList({"b":2,"bl":1,"br":1,"c":2,"cr":1,"ch":2,"cl":1,"d":2,"f":2,"fl":1,"fr":1,"g":2,"gl":1,"gr":1,"h":1,"j":2,"k":2,"l":2,"m":3,"n":2,"p":2,"pl":1,"pr":1,"qu":1,"r":2,"s":3,"sh":2,"sk":1,"sl":1,"sm":1,"sn":1,"st":2,"str":1,"t":3,"th":2,"thr":1,"tr":2,"tw":1,"v":2,"w":1,"z":2}),
+    c2: GenPwd.RandomList(["b","bl","br","cr","ch","cl","d","f","fl","fr","g","gg", "gl","gr","h","j","k","l","m","n","p","pl","pp","pr","pt","qu","r","s","sh","sk","sl","sm","sn","st","str","t","th","thr","tr","tw","v","w","z"]),
+    c3: GenPwd.WeightedList({"b":1,"ch":1,"ck":1,"ct":1,"d":2,"dd":1,"f":1,"ff":1,"ft":1,"g":1,"k":1,"l":2,"ll":1,"lb":1,"ld":1,"lm":1,"ln":1,"lp":1,"lt":1,"m":3,"mp":1,"mt":1,"n":3,"nd":1,"ng":1,"nn":1,"nt":1,"p":2,"pp":1,"pt":1,"rd":1,"rg":1,"rk":1,"rn":1,"rr":1,"rs":1,"rt":1,"s":3,"sh":1,"ss":2,"st":2,"t":3,"tt":2,"th":2,"v":2,"wn":1}),
+    v1: GenPwd.WeightedList({"a":5,"aa":1,"ai":1,"e":5,"ea":1,"ee":1,"i":5,"o":5,"oo":2,"u":2}),
+    v2: GenPwd.WeightedList({"a":5, "e":5,"i":5, "ia":1, "o":5, "oa":1,"oo":2, "u":2, "ua":1}),
+    v3: GenPwd.WeightedList({"a":5,"ao":1,"e":5,"ea":1,"ee":2,"eo":1,"i":2,"ia":2,"io":2,"o":5,"oa":2,"oo":2,"ow":2,"ua":1,"uo":1,"y":5}),
+    p:  GenPwd.RandomList(["!","#","$","^","*","&", "+","@","-","=","/","~","?","\\","%","[","]","{","}","(",")"]),
     cap: function (s) { return ($("#capitals:checked").length > 0 ? _.capitalize(s) : s); },
     punc: function () { return ($("#punctuation:checked").length > 0 ? this.p() : ""); },
     num: function () { return ($("#numbers:checked").length > 0 ? _.padStart(_.random(0, 99), 2, "0") : ""); },
@@ -99,10 +109,10 @@ var Generator = {
 
   // Pseudo-Japanese style
   generator2: {
-    c1: WeightedList({"k":5,"g":5,"t":5,"d":5,"s":5,"z":4,"h":2,"b":3,"p":2,"n":3,"r":5,"m":5,"y":2,
+    c1: GenPwd.WeightedList({"k":5,"g":5,"t":5,"d":5,"s":5,"z":4,"h":2,"b":3,"p":2,"n":3,"r":5,"m":5,"y":2,
                      "gy":1,"j":1,"sh":1,"ch":1,"ky":1,"hy":1,"ry":1,"my":1,"ny":1,"by":1,"py":1}),
-    v1: WeightedList({"a":2,"i":1,"u":2,"e":1,"o":2, "ou":1}),
-    p:  RandomList(["!","#","$","^","*","&", "+","@","-","=","/","~","?","\\","%","[","]","{","}","(",")"]),
+    v1: GenPwd.WeightedList({"a":2,"i":1,"u":2,"e":1,"o":2, "ou":1}),
+    p:  GenPwd.RandomList(["!","#","$","^","*","&", "+","@","-","=","/","~","?","\\","%","[","]","{","}","(",")"]),
     cap: function (s) { return ($("#capitals:checked").length > 0 ? _.capitalize(s) : s); },
     punc: function () { return ($("#punctuation:checked").length > 0 ? this.p() : ""); },
     num: function () { return ($("#numbers:checked").length > 0 ? _.padStart(_.random(0, 99), 2, "0") : ""); },
@@ -133,23 +143,15 @@ var Generator = {
 
   // Public methods
 
-  // Get the state of a form element
-  getState: function (id) {
-    return $(id).attr("checked");
-  },
-
   generate: function () {
     var gen = eval("Generator." + $("#gen").val());
     $("#output").empty();
-    for (var i=0; i < nwords; i++) {
+    for (var i=0; i < GenPwd.nwords; i++) {
       elt = $("<div class='word'></div>").append(gen.randomWord());
       $("#output").append(elt);
     }
   },
 
-  initialise: function () {
-    Info.appendTo("header");
-  }		
 }			
 
 // The End
