3d2e1b7315d99b03d274c324d029365d6292e0ee
AndrewJ <web@alphajuliet.com>
2016-06-25 20:54:53 +1000

    Automatically populate list of generators in HTML page from exported list in Generator namespace. Split long lines in generators. Move generate() to GenPwd namespace.

--- build/GenPwd.js
+++ build/GenPwd.js
@@ -15,7 +15,7 @@ GenPwd = (function () {
   var Info = {
     name: "GenPwd",
     author: "AndrewJ", 
-    version: "2.15",
+    version: "2.16",
     date: "2016-06-25",
     info: "GenPwd is a very simple password generator.",
     appendTo: function(tagName) {
@@ -59,7 +59,24 @@ GenPwd = (function () {
   };
 
   var initialise = function () {
+    var fn;
     Info.appendTo("header");
+    $.each(Generator.generators, function (i, gen) {
+      if (gen.default == true) 
+        fn = gen.fn + '" selected="true';
+      else
+        fn = gen.fn
+      $('#gen').append($('<option value="' + fn + '">' + gen.name + '</option>'))
+    });
+  };
+
+  var generate = function () {
+    var gen = eval('Generator.' + $('#gen').val());
+    $("#output").empty();
+    for (var i=0; i < nwords; i++) {
+      elt = $("<div class='word'></div>").append(gen.randomWord());
+      $("#output").append(elt);
+    }
   };
 
   // Public
@@ -68,7 +85,8 @@ GenPwd = (function () {
     randomNumericString: randomNumericString,
     RandomList: RandomList,
     WeightedList: WeightedList,
-    initialise: initialise
+    initialise: initialise,
+    generate: generate
   }
 
 })();
@@ -82,14 +100,31 @@ Generator = (function () {
 
   // Generators
   var generator1 = {	
-    name: "Generator 1",
-    c1: GenPwd.WeightedList({"b":2,"bl":1,"br":1,"c":2,"cr":1,"ch":2,"cl":1,"d":2,"f":2,"fl":1,"fr":1,"g":2,"gl":1,"gr":1,"h":1,"j":2,"k":2,"l":2,"m":3,"n":2,"p":2,"pl":1,"pr":1,"qu":1,"r":2,"s":3,"sh":2,"sk":1,"sl":1,"sm":1,"sn":1,"st":2,"str":1,"t":3,"th":2,"thr":1,"tr":2,"tw":1,"v":2,"w":1,"z":2}),
-    c2: GenPwd.RandomList(["b","bl","br","cr","ch","cl","d","f","fl","fr","g","gg", "gl","gr","h","j","k","l","m","n","p","pl","pp","pr","pt","qu","r","s","sh","sk","sl","sm","sn","st","str","t","th","thr","tr","tw","v","w","z"]),
-    c3: GenPwd.WeightedList({"b":1,"ch":1,"ck":1,"ct":1,"d":2,"dd":1,"f":1,"ff":1,"ft":1,"g":1,"k":1,"l":2,"ll":1,"lb":1,"ld":1,"lm":1,"ln":1,"lp":1,"lt":1,"m":3,"mp":1,"mt":1,"n":3,"nd":1,"ng":1,"nn":1,"nt":1,"p":2,"pp":1,"pt":1,"rd":1,"rg":1,"rk":1,"rn":1,"rr":1,"rs":1,"rt":1,"s":3,"sh":1,"ss":2,"st":2,"t":3,"tt":2,"th":2,"v":2,"wn":1}),
-    v1: GenPwd.WeightedList({"a":5,"aa":1,"ai":1,"e":5,"ea":1,"ee":1,"i":5,"o":5,"oo":2,"u":2}),
-    v2: GenPwd.WeightedList({"a":5, "e":5,"i":5, "ia":1, "o":5, "oa":1,"oo":2, "u":2, "ua":1}),
-    v3: GenPwd.WeightedList({"a":5,"ao":1,"e":5,"ea":1,"ee":2,"eo":1,"i":2,"ia":2,"io":2,"o":5,"oa":2,"oo":2,"ow":2,"ua":1,"uo":1,"y":5}),
-    p:  GenPwd.RandomList(["!","#","$","^","*","&", "+","@","-","=","/","~","?","\\","%","[","]","{","}","(",")"]),
+    name: "English 1",
+    c1: GenPwd.WeightedList(
+      {"b":2,"bl":1,"br":1,"c":2,"cr":1,"ch":2,"cl":1,"d":2,"f":2,"fl":1,
+        "fr":1,"g":2,"gl":1,"gr":1,"h":1,"j":2,"k":2,"l":2,"m":3,"n":2,"p":2,
+        "pl":1,"pr":1,"qu":1,"r":2,"s":3,"sh":2,"sk":1,"sl":1,"sm":1,"sn":1,
+        "st":2,"str":1,"t":3,"th":2,"thr":1,"tr":2,"tw":1,"v":2,"w":1,"z":2}),
+    c2: GenPwd.RandomList(
+      ["b","bl","br","cr","ch","cl","d","f","fl","fr","g","gg", "gl","gr",
+        "h","j","k","l","m","n","p","pl","pp","pr","pt","qu","r","s","sh","sk",
+        "sl","sm","sn","st","str","t","th","thr","tr","tw","v","w","z"]),
+    c3: GenPwd.WeightedList(
+      {"b":1,"ch":1,"ck":1,"ct":1,"d":2,"dd":1,"f":1,"ff":1,"ft":1,"g":1,
+        "k":1,"l":2,"ll":1,"lb":1,"ld":1,"lm":1,"ln":1,"lp":1,"lt":1,"m":3,
+        "mp":1,"mt":1,"n":3,"nd":1,"ng":1,"nn":1,"nt":1,"p":2,"pp":1,"pt":1,
+        "rd":1,"rg":1,"rk":1,"rn":1,"rr":1,"rs":1,"rt":1,"s":3,"sh":1,"ss":2,
+        "st":2,"t":3,"tt":2,"th":2,"v":2,"wn":1}),
+    v1: GenPwd.WeightedList(
+      {"a":5,"aa":1,"ai":1,"e":5,"ea":1,"ee":1,"i":5,"o":5,"oo":2,"u":2}),
+    v2: GenPwd.WeightedList(
+      {"a":5, "e":5,"i":5, "ia":1, "o":5, "oa":1,"oo":2, "u":2, "ua":1}),
+    v3: GenPwd.WeightedList(
+      {"a":5,"ao":1,"e":5,"ea":1,"ee":2,"eo":1,"i":2,"ia":2,"io":2,"o":5,
+        "oa":2,"oo":2,"ow":2,"ua":1,"uo":1,"y":5}),
+    p:  GenPwd.RandomList(
+      ["!","#","$","^","*","&", "+","@","-","=","/","~","?","\\","%","[","]","{","}","(",")"]),
     cap: function (s) { return ($("#capitals:checked").length > 0 ? _.capitalize(s) : s); },
     punc: function () { return ($("#punctuation:checked").length > 0 ? this.p() : ""); },
     num: function () { return ($("#numbers:checked").length > 0 ? GenPwd.randomNumericString() : ""); },
@@ -116,11 +151,16 @@ Generator = (function () {
 
   // Pseudo-Japanese style
   var generator2 = {
-    name: "Generator 2",
-    c1: GenPwd.WeightedList({"k":5,"g":5,"t":5,"d":5,"s":5,"z":4,"h":2,"b":3,"p":2,"n":3,"r":5,"m":5,"y":2,
-                     "gy":1,"j":1,"sh":1,"ch":1,"ky":1,"hy":1,"ry":1,"my":1,"ny":1,"by":1,"py":1}),
-    v1: GenPwd.WeightedList({"a":2,"i":1,"u":2,"e":1,"o":2, "ou":1}),
-    p:  GenPwd.RandomList(["!","#","$","^","*","&", "+","@","-","=","/","~","?","\\","%","[","]","{","}","(",")"]),
+    name: "Japanese",
+    c1: GenPwd.WeightedList(
+      {"k":5,"g":5,"t":5,"d":5,"s":5,"z":4,"h":3,"b":3,
+        "p":3,"n":3,"r":5,"m":5,"y":2,"gy":1,"j":2,"sh":2,
+        "ch":2,"ky":1,"hy":1,"ry":2,"my":1,"ny":1,"by":1,"py":1}),
+    v1: GenPwd.WeightedList(
+      {"a":2,"i":1,"u":2,"e":1,"o":2, "ou":1}),
+    p:  GenPwd.RandomList(
+      ["!","#","$","^","*","&", "+","@","-","=","/","~","?",
+        "\\","%","[","]","{","}","(",")"]),
     cap: function (s) { return ($("#capitals:checked").length > 0 ? _.capitalize(s) : s); },
     punc: function () { return ($("#punctuation:checked").length > 0 ? this.p() : ""); },
     num: function () { return ($("#numbers:checked").length > 0 ? GenPwd.randomNumericString() : ""); },
@@ -135,12 +175,13 @@ Generator = (function () {
         return x
       };
 
-      c = _.random(0, 2);
+      c = _.random(0, 4);
       switch (c) {
         case 0: w = syll() + R.punc() + R.cap(syll()); break;
-        case 1: w = syll() + R.punc() + R.cap(syll()) + syll(); break;
+        case 1: w = syll() + R.punc() + syll() + R.cap(syll()); break;
         case 2: w = R.v1() + R.cap(syll()) + R.punc() + syll() ; break;
-        default: w = R.punc() + syll() + R.cap(syll());
+        case 3: w = syll() + R.v1() + R.cap(syll()) + R.punc(); break;
+        default: w = syll() + R.cap(syll()) + R.punc();
       }
       w = (_.random(0, 2) < 1) ? w + R.num() : R.num() + w; // add a number at start or end
       w = w.replace(/[Tt]i/, "chi");
@@ -155,20 +196,14 @@ Generator = (function () {
     },
   };
 
-  var generate = function () {
-    var gen = eval("Generator." + $("#gen").val());
-    $("#output").empty();
-    for (var i=0; i < GenPwd.nwords; i++) {
-      elt = $("<div class='word'></div>").append(gen.randomWord());
-      $("#output").append(elt);
-    }
-  };
-
   // Public
   return {
+    generators: [
+      {"name": "English 1", "fn": "generator1"}, 
+      {"name": "Japanese", "fn": "generator2", "default": true}
+    ],
     generator1: generator1,
     generator2: generator2,
-    generate: generate
   }
 
 })();	
